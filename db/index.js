9d8e82307bd9cdd9d04dfa53092a752f64dd0ae8
sean w <52753311+vtsu3@users.noreply.github.com>
2020-01-25 01:10:02 -0800

    successfully seeded 5 documents to database

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 // Setup
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb://localhost:27017/SoundClout',
+  'mongodb://localhost:27017/soundclone',
   {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -20,7 +20,7 @@ var commentSchema = mongoose.Schema({
         followCount: Number
       },
       timeData: {
-        postDate: Date,
+        postDate: String,
         timestamp: Number
       },
       commentBody: String
@@ -28,23 +28,73 @@ var commentSchema = mongoose.Schema({
   ],
 });
 
-// Compile the model from the schema
-const commentModel = mongoose.model('commentModel', commentSchema);
+// Compile and export the model
+const commentModel = mongoose.model('commentdocuments', commentSchema);
+module.exports = commentModel;
 
+// Seed database
 commentModel.create({
   songId: 1,
   comments: [
     {
       userData: {
+        username: 'destiny',
+        profilePicture: 'https://i.redd.it/kqgh408o6tc41.jpg',
+        followCount: 194824
+      },
+      timeData: {
+        postDate: '1/24/20',
+        timestamp: 104
+      },
+      commentBody: 'this is an awesome song bro, keep up these bopping beats and those rickety rhymes!'
+    },
+    {
+      userData: {
         username: 'vtsu3',
-        profilePicture: 'testurl.com',
+        profilePicture: 'https://i.redd.it/kqgh408o6tc41.jpg',
         followCount: 40
       },
       timeData: {
         postDate: '1/24/20',
-        timestamp: 10
+        timestamp: 42
       },
-      commentBody: 'hello my friend! this is an awesome song bro, keep up the bopping beats and the rickety rhymes!'
+      commentBody: 'epic song mate'
+    },
+    {
+      userData: {
+        username: 'tarzaned',
+        profilePicture: 'https://i.redd.it/kqgh408o6tc41.jpg',
+        followCount: 9914
+      },
+      timeData: {
+        postDate: '1/24/20',
+        timestamp: 1
+      },
+      commentBody: 'rank1 s6 btw'
+    },
+    {
+      userData: {
+        username: 'kang1099jt',
+        profilePicture: 'https://i.redd.it/kqgh408o6tc41.jpg',
+        followCount: 64
+      },
+      timeData: {
+        postDate: '1/14/20',
+        timestamp: 120
+      },
+      commentBody: 'sweet tunes brother, what shampoo do you use?'
+    },
+    {
+      userData: {
+        username: 'tyler1',
+        profilePicture: 'https://static-cdn.jtvnw.net/jtv_user_pictures/f3591dbe4ee3d94b-profile_image-70x70.png',
+        followCount: 9194205
+      },
+      timeData: {
+        postDate: '1/23/20',
+        timestamp: 3
+      },
+      commentBody: 'THAIYS IS A FUJKCOIRN TRSAH SONG HAHASFHUDAHAH'
     }
   ],
 })
