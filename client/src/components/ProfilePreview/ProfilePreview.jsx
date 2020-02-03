f87179c95992c88c50f83030007021b378642d98
sean w <52753311+vtsu3@users.noreply.github.com>
2020-02-03 00:55:35 -0800

    refactor jest commit

--- client/src/components/ProfilePreview/ProfilePreview.jsx
+++ client/src/components/ProfilePreview/ProfilePreview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import debounce from 'lodash';
 
 class ProfilePreview extends React.Component {
   constructor(props) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
 const path = require('path');
-
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+  'mode': 'development',
+  'entry': `${SRC_DIR}/index.jsx`,
+  'output': {
+    'path': DIST_DIR,
+    'filename': 'bundle.js'
   },
-  module: {
-    rules: [
+  'devtool': 'source-map',
+  'module': {
+    'rules': [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-react', '@babel/preset-env']
+        'test': /\.(js|jsx)$/,
+        'exclude': /node_modules/,
+        'use': {
+          'loader': 'babel-loader',
+          'options': {
+            'presets': [
+              '@babel/preset-env',
+              '@babel/react'
+            ]
+          }
         }
+      },
+      {
+        'test': /\.css$/,
+        'use': [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   }
-};
+}
