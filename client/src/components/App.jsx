301d3adfed27a573a6f29d5aafb8b03a61b1e706
sean w <52753311+vtsu3@users.noreply.github.com>
2020-02-05 10:41:56 -0800

    reverted and cleaned up code to working state :(++

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import CmtList from './CmtList.jsx';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  padding-top: 40px;
+  font: 12px/1.4 'Lucida Grande';
+  color: #333;
+`;
 
 const App = () => {
   return (
-    <div>
+    <AppContainer>
       <CmtList id="CmtList"/>
-    </div>
+    </AppContainer>
   );
 };
 
