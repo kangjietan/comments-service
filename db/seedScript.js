1b009b87c7703613dc0a49ce5edfc91c65a8824a
sean w <52753311+vtsu3@users.noreply.github.com>
2020-02-05 12:08:56 -0800

    added more comments

--- db/seedScript.js
+++ db/seedScript.js
@@ -1,80 +1,53 @@
 const commentModel = require('./index.js');
 var faker = require('faker');
 
+class CommentGen {
+  constructor() {
+    this.data = {
+      userData: {
+        username: faker.internet.userName(),
+        displayName: faker.name.findName(),
+        profileURL: 'https://soundcloud.com/richarddjames',
+        profilePicture: faker.image.avatar(),
+        followCount: faker.random.number(),
+      },
+      timeData: {
+        postDate: `${this.monthGen()}/${this.dateGen()}/${this.yearGen()}`,
+        timestamp: faker.random.number(180),
+      },
+      commentBody: faker.lorem.sentences()
+    };
+  }
+
+  dateGen () {
+    let max = 30;
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  monthGen () {
+    let max = 13;
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  yearGen () {
+    return faker.random.number( {min: 2018, max: 2020} );
+  }
+}
+
+let output = (z) => {
+  let res = [];
+
+  for (let i = 0; i < z; i++) {
+    let temp = new CommentGen();
+    res.push(temp.data);
+  }
+
+  return res;
+};
+
 commentModel.create({
   songId: 1,
-  comments: [
-    {
-      userData: {
-        username: 'DESTINY55',
-        displayName: faker.name.findName(),
-        profileURL: 'https://soundcloud.com/shibasan',
-        profilePicture: faker.image.avatar(),
-        followCount: 194824,
-      },
-      timeData: {
-        postDate: '1/24/20',
-        timestamp: faker.random.number(180),
-      },
-      commentBody: faker.lorem.sentences(),
-    },
-    {
-      userData: {
-        username: 'vtsu3',
-        displayName: faker.name.findName(),
-        profileURL: 'https://soundcloud.com/shibasan',
-        profilePicture: faker.image.avatar(),
-        followCount: 40,
-      },
-      timeData: {
-        postDate: '1/24/20',
-        timestamp: faker.random.number(180),
-      },
-      commentBody: faker.lorem.sentences(),
-    },
-    {
-      userData: {
-        username: 'hongkongbboy',
-        displayName: faker.name.findName(),
-        profileURL: 'https://soundcloud.com/shibasan',
-        profilePicture: faker.image.avatar(),
-        followCount: 9914,
-      },
-      timeData: {
-        postDate: '1/24/20',
-        timestamp: faker.random.number(180),
-      },
-      commentBody: faker.lorem.sentences(),
-    },
-    {
-      userData: {
-        username: 'kang1099jt',
-        displayName: faker.name.findName(),
-        profileURL: 'https://soundcloud.com/shibasan',
-        profilePicture: faker.image.avatar(),
-        followCount: 64,
-      },
-      timeData: {
-        postDate: '1/14/20',
-        timestamp: faker.random.number(180),
-      },
-      commentBody: faker.lorem.sentences(),
-    },
-    {
-      userData: {
-        username: 'tyler1',
-        displayName: 'Tyler Steinkamp One',
-        profileURL: 'https://soundcloud.com/shibasan',
-        profilePicture: 'https://static-cdn.jtvnw.net/jtv_user_pictures/f3591dbe4ee3d94b-profile_image-70x70.png',
-        followCount: 9194205,
-      },
-      timeData: {
-        postDate: '1/23/20',
-        timestamp: faker.random.number(180),
-      },
-      commentBody: faker.lorem.sentences(),
-    },
-  ],
+  comments: output(35)
 });
 
 console.log('The database has been populated.');
