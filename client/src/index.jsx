ab79372f2a414b7a9b8079b2cb2eab21a49437d8
Kang Jie Tan <kangjietan@gmail.com>
2020-02-07 19:44:33 -0800

    Move into new folder

--- client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-// DO NOT MODIFY
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root'));
