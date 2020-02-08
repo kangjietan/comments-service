593d3175b7e0f11449d806641a10edb8c23c1ab7
Kang Jie Tan <kangjietan@gmail.com>
2020-02-07 20:22:03 -0800

    Change to right route

--- service/server/index.js
+++ service/server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(morgan('dev'));
 
 app.get('/bundle.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/bundle.js'));
+  res.sendFile(path.join(__dirname, '../client/public/bundle.js'));
 });
 
 // GET requests (routes/routesGET.js)
